9f192242a967adbd5f2cd76e697b445929d6d7d1
Taylor Silva <taylorsilva93@live.com>
2014-04-05 16:27:29 -0400

    Users can create podcasts

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,9 +1,29 @@
 class PodcastsController < ApplicationController
+	respond_to :html, :json, :xml
+
 	def new
-		@podcast = Podcast.new(user_id: params[:user_id])
+		@user = User.find(params[:user_id])
+		@podcast = @user.podcasts.build
+		respond_with(@podcast)
+	end
+
+	def create
+		@user = User.find(params[:user_id])
+		@podcast = @user.podcasts.build(podcast_params)
+		if @podcast.save
+			flash[:success] = "Podcast created"
+			redirect_to root_url
+		else
+			render 'new'
+		end
 	end
 
 	def index
 		@podcast = Podcast.all
 	end
+
+	private
+		def podcast_params
+			params.require(:podcast).permit(:name, :description)
+		end	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	#attr_accessible :user_id
 	has_many :podcasts, dependent: :destroy
 	before_create :create_remember_token
 	before_save { self.email = email.downcase }
