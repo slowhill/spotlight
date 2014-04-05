b924bf2b5a776a81b15d0880ebeaa1e3be479680
Dustin Woo <dustin.woo@outlook.com>
2014-04-04 21:54:58 -0400

    Initial commit: generated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spotlight::Application.config.secret_key_base = '9804c72a224f4dbf142107dae35b3d683bb3431d1d0718e8e91ce0a1c7706d09fae57924cc04d827b4cddd68f178decc32b7a08752ea3a9d92d03b56ca47affb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spotlight::Application.config.session_store :cookie_store, key: '_spotlight_session'
