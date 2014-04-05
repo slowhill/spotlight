14df591e5b4b0344011864fe9a888ddac0d6a816
Dustin Woo <dustin.woo@outlook.com>
2014-04-05 03:29:14 -0400

    Modified users table, added validation to users model. RUN RAKE DB:MIGRATE

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,2 +1,12 @@
 class Users < ActiveRecord::Base
+	validates :fname, presence: true
+	validates :lname, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+				uniqueness: { case_sensitive: false }
+	validates :username, presence: true
+	validates :password, presence: true, length: { minimum: 8 }
+
+	before_save { self.email = email.downcase }
+	has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140405030658) do
+ActiveRecord::Schema.define(version: 20140405071350) do
 
   create_table "episodes", force: true do |t|
     t.integer  "podcast_id"
@@ -58,9 +58,11 @@ ActiveRecord::Schema.define(version: 20140405030658) do
     t.string   "lname"
     t.string   "email"
     t.string   "username"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
