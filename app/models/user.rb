28ea04bfafc1c25f77e64307ee712d53f9baf85b
Dustin Woo <dustin.woo@outlook.com>
2014-04-05 09:53:10 -0400

    Made signup error message a partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 				uniqueness: { case_sensitive: false }
 	validates :username, presence: true
-	validates :password, presence: true, length: { minimum: 8 }
+	validates :password, length: { minimum: 8 }
 
 	has_secure_password
 end
