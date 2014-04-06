670527144fa5b7295b36ad8b34db4d2f3a99dc3c
Dustin Woo <dustin.woo@outlook.com>
2014-04-06 03:27:32 -0400

    episode controller new/create templated

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -9,11 +9,20 @@ class EpisodesController < ApplicationController
 
 	def create
 		@podcast = User.find(params[:user_id]).podcasts.find(params[:podcast_id])
-		@episode = @podcast.episodes.build
+		@episode = @podcast.episodes.build(epcreate_params)
+		if @episode.save 
+			flash.now[:success] = "3, 2, 1.."
+			redirect_to 'show'
+		else
+			render 'new'
+		end
+	end
+
+	def show
 	end
 
 	private
-		def podcast_params
-			params.require(:episode).permit(:title, :description, :upvote_type)
+		def epcreate_params
+			params.require(:episode).permit(:title, :description, :upvote_type, :min_upvotes)
 		end	
 end
