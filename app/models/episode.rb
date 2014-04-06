de9d18145131c8cb4f7cff0ef84bacaea677a0ad
Dustin Woo <dustin.woo@outlook.com>
2014-04-05 20:28:16 -0400

    Designed podcast page a little more, defined relationship between podcast & episodes in model

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,3 @@
 class Episode < ActiveRecord::Base
+	belongs_to :podcast, dependent: :destroy
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,5 +1,6 @@
 class Podcast < ActiveRecord::Base
 	belongs_to :user
+	has_many :episodes
 	validates :user_id, presence: true
 	validates :name, presence: true, length: { maximum: 100 }
 	validates :description, presence: true, length: { maximum: 140 }	
