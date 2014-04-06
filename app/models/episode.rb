535b0a4321b9bfb269b5d7873a0172e1b14cec00
Dustin Woo <dustin.woo@outlook.com>
2014-04-05 21:40:15 -0400

    Episodes go down by desc order, episodes controller generated

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,6 @@
 class Episode < ActiveRecord::Base
 	belongs_to :podcast, dependent: :destroy
+	default_scope -> { order('created_at DESC') }
+	validates :title, presence: true, length: { maximum: 100 }
+	validates :description, presence: true, length: { maximum: 500 }
 end
